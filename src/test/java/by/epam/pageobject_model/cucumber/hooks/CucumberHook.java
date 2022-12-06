228eab865f5bebb98138d0e8b7697cf0bd95a6af
Natalya Gumen <natalya_gumen@gmail.com>
2022-12-06 23:51:43 +0300

    clean cucumber

--- src/test/java/by/epam/pageobject_model/cucumber/hooks/CucumberHook.java
+++ src/test/java/by/epam/pageobject_model/cucumber/hooks/CucumberHook.java
@@ -2,25 +2,13 @@ package by.epam.pageobject_model.cucumber.hooks;
 
 import by.epam.pageobject_model.driver.DriverSingleton;
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
 public class CucumberHook {
     protected WebDriver driver;
-    protected final Logger LOGGER = LogManager.getRootLogger();
-
-    @Before
-    public void seleniumDriverSetup() {
-        driver = DriverSingleton.getDriver();
-        LOGGER.info("Cucumber getDriver");
-
-    }
 
     @After
     public void driverTearDown() {
         DriverSingleton.closeDriver();
-        LOGGER.info("Cucumber closeDriver");
     }
 }
--- src/test/java/by/epam/pageobject_model/cucumber/runner/TestRunner.java
+++ src/test/java/by/epam/pageobject_model/cucumber/runner/TestRunner.java
@@ -3,9 +3,6 @@ package by.epam.pageobject_model.cucumber.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-import org.testng.annotations.Listeners;
-import by.epam.pageobject_model.utils.TestListener;
-@Listeners({TestListener.class})
 @CucumberOptions(
         features = {"src/test/resources/loginMailru.feature","src/test/resources/mailru.feature"},
         glue={"by.epam.pageobject_model.cucumber.steps","by.epam.pageobject_model.cucumber.hooks"},
