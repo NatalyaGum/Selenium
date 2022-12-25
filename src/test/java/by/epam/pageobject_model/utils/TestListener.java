1a2ee267f2defe602f0cdfb9f0611412ef432f14
Natalya Gumen <natalya_gumen@gmail.com>
2022-12-26 02:23:13 +0300

    rp

--- src/test/java/by/epam/pageobject_model/utils/TestListener.java
+++ src/test/java/by/epam/pageobject_model/utils/TestListener.java
@@ -1,6 +1,7 @@
 package by.epam.pageobject_model.utils;
 
 import by.epam.pageobject_model.driver.DriverSingleton;
+import com.epam.reportportal.message.ReportPortalMessage;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,20 +12,16 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import java.io.File;
-import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-
 
 public class TestListener implements ITestListener {
     private final Logger LOGGER = LogManager.getRootLogger();
 
     public void onTestStart(ITestResult iTestResult) {
-
     }
 
     public void onTestSuccess(ITestResult iTestResult) {
-
     }
 
     public void onTestFailure(ITestResult iTestResult) {
@@ -33,32 +30,39 @@ public class TestListener implements ITestListener {
     }
 
     public void onTestSkipped(ITestResult iTestResult) {
-
+        try { File screenCapture = ((TakesScreenshot) DriverSingleton
+                .getDriver())
+                .getScreenshotAs(OutputType.FILE);
+            File copy=new File(".//target/screenshots/"
+                    + getCurrentTimeAsString() +
+                    ".png");
+            FileUtils.copyFile(screenCapture, copy);
+            LOGGER.error("RP_MESSAGE#FILE#{}#{}", copy.getAbsolutePath()," - Saved screenshot");
+        } catch (Exception e) {
+            LOGGER.error("Failed: " + e.getMessage());
+        }
     }
 
     public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-
     }
 
     public void onStart(ITestContext iTestContext) {
-
     }
 
     public void onFinish(ITestContext iTestContext) {
-
     }
 
     private void saveScreenshot() {
         try { File screenCapture = ((TakesScreenshot) DriverSingleton
                 .getDriver())
                 .getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(screenCapture, new File(
-                    ".//target/screenshots/"
+            File copy=new File(".//target/screenshots/"
                             + getCurrentTimeAsString() +
-                            ".png"));
-
+                            ".png");
+            FileUtils.copyFile(screenCapture, copy);
+            LOGGER.error("RP_MESSAGE#FILE#{}#{}", copy.getAbsolutePath()," - Saved screenshot");
         } catch (Exception e) {
-            LOGGER.error("Failed to save screenshot: " + e.getMessage());
+            LOGGER.error("Failed: " + e.getMessage());
         }
     }
 
